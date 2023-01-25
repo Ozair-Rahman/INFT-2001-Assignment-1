30fd32eabfeaf097ac0fce566ed4d8e0c3bf7b92
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-01-25 16:25:44 -0500

    Did Changes

--- Code/donut-website/src/App.js
+++ Code/donut-website/src/App.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import './App.css';
->>>>>>> ac0a6b39a870826d62cf4cee1ab0c2683c8298ef
 import AboutComponent from './Components/About';
 import ProductComponent from './Components/Product';
 import HomeComponent from './Components/Home';
@@ -10,6 +6,7 @@ import ContactComponent from './Components/Contact';
 function App() {
   return (
     <div className="App">
+      <a href='./Components/About'>About Page</a>
       <HomeComponent />
       <AboutComponent />
       <ContactComponent />
