1295f76e5ecaa90a6f159b1839338fc57a87c166
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-01-25 21:39:11 -0500

    Product Component: Added Dedicated CSS File to Product Component

---
+++ Code/donut-website/src/Components/Product Component/OptionOneComponent.jsx
@@ -0,0 +1,9 @@
+const OptionOneComponent = () => {
+    return ( 
+        <div className="optionOneComponentContainer">
+            <h1>Hello World</h1>
+        </div>
+     );
+}
+ 
+export default OptionOneComponent;
