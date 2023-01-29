82e6ab6bdecb4aaac4785e06ee7d98f612ff360d
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-01-29 17:17:26 -0500

    Finished Error Page

--- Code/donut-website/src/Components/Contact Component/Contact.jsx
+++ Code/donut-website/src/Components/Contact Component/Contact.jsx
@@ -6,18 +6,11 @@ const ContactComponent = () => {
   document.title = 'Contact Us - Crave Doughnuts';
 
   return (
-    <div className="ContactComponentContent">
+    <>
       <NavBar />
-      <h1>Contact Us</h1>
-      <h2>Name</h2>
-      <input className="name" type="text" />
-      <h2>Subject</h2>
-      <input className="subject" type="text" />
-      <h2>Message</h2>
-      <textarea className="message" rows="4" cols="50"></textarea>
-      <button>Send</button>
+      <header>Contact Us!</header>
       <Footer />
-    </div>
+    </>
   );
 };
 
--- Code/donut-website/src/Components/Error Page Component/ErrorComponent.jsx
+++ Code/donut-website/src/Components/Error Page Component/ErrorComponent.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Footer from '../Footer Component/Footer';
 import NavBar from '../Navbar Component/NavBar';
 import './ErrorComponent.css';
@@ -9,7 +10,13 @@ const ErrorComponent = () => {
     return ( 
         <>
             <NavBar />
-            <h1>Error 404 - No Page Found</h1>
+            <div className="erroMsg">
+                <h1 className='errorHeading'>Error 404 - No Page Found</h1>
+                <p className='errorSubHeading'>The page you requested does not exist.</p>
+                <Link to='/' className='homeLink'>
+                    <p className='homeLinkText'>Continue Shopping</p>
+                </Link>
+            </div>
             <Footer />
         </>
      );
