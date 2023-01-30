c6cd701c4a344ac9528a77b1a382ef42dd3b725b
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-01-30 01:50:42 -0500

    Added Email Capabiliy to Contact Page

--- Code/donut-website/src/Components/Contact Component/Contact.jsx
+++ Code/donut-website/src/Components/Contact Component/Contact.jsx
@@ -1,14 +1,39 @@
+import './Contact.css';
 import Footer from "../Footer Component/Footer";
 import NavBar from "../Navbar Component/NavBar";
+import emailjs from 'emailjs-com';
 
 const ContactComponent = () => {
 
   document.title = 'Contact Us - Crave Doughnuts';
 
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8q1rbts', 'template_6ap0rgc', e.target, '-05ct2oJjHgRWMZUy')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();  
+  }
+
   return (
     <>
       <NavBar />
-      <header>Contact Us!</header>
+        <div className="emailFormDiv">
+          <header className='emailFormHeader'>Contact Us!</header>
+          <form onSubmit={sendEmail} className='emailForm'>
+            <label className='nameLabel'>Name:</label>
+            <input type="text" name="user_name" />
+            <label className='emailLabel'>Email:</label>
+            <input type="email" name="user_email" />
+            <label className='msgLabel'>Message:</label>
+            <textarea className='msgBox' name="message" />
+            <input type="submit" value="Send" className='sendBtn' />
+          </form>
+        </div>
       <Footer />
     </>
   );
--- Code/donut-website/src/Components/Footer Component/Footer.jsx
+++ Code/donut-website/src/Components/Footer Component/Footer.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import './Footer.css'
  
 const Footer = () => {
+
     return ( 
         <>
             <footer className="homepageFooter">
