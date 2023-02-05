12168a1ff540cec678ab12aa8117722b1eacafe2
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-02-05 15:15:21 -0500

    Product Component: Finished Comments for Product Component

--- Code/donut-website/src/Components/Product Component/Product.jsx
+++ Code/donut-website/src/Components/Product Component/Product.jsx
@@ -1,101 +1,109 @@
 // Imports
-import './Styles/Product.css';
-import { Link } from 'react-router-dom';
-import NavBar from '../Navbar Component/NavBar';
-import Footer from '../Footer Component/Footer';
+import './Styles/Product.css'; // CSS
+import { Link } from 'react-router-dom'; // Import Link to Navigate Between Pages
+import NavBar from '../Navbar Component/NavBar'; // NavBar
+import Footer from '../Footer Component/Footer'; // Footer
 
 const ProductComponent = () => {
 
     document.title = 'Doughnuts - Crave Doughnuts'; // Change Title When On Product Page
 
-    const optionOneThreePrice = '$24.00';
-    const optionTwoPrice = '$44.00';
+    const optionOneThreePrice = '$24.00'; // Defined Price as Var As to Not Repeat Code
 
     return ( 
         <div className="ProductComponentContent">
-            <NavBar />
-                <div className="optionContainer">
-                    <Link to="/productOne" className='productLink'>
-                        <div className="optionOneOption">
-                            <h1>
-                                Option 1: Half Dozen Donuts - 
-                                Flavors: 
-                                Birthday Cake
-                                Lemon Meringue
-                                French Toast
-                                Lemon Sprinkle
-                                Cosmic Brownie
-                                Honey Vanilla Cruller
-                            </h1>
-                        </div>
-                        <h2 className="optionTwoPriceTitle">Crave Option 1 - Half Dozen</h2>
-                        <h3 className="optionTwoPrice">{optionOneThreePrice}</h3>
-                    </Link>
+            <NavBar /> {/* NavBar */}
+
+            {/* Option 1 Section */}
+            <div className="optionContainer">
+                <Link to="/productOne" className='productLink'> {/* Link to ProductOne Component */}
+                    <div className="optionOneOption">
+                        <h1>
+                            Option 1: Half Dozen Donuts - 
+                            Flavors: 
+                            Birthday Cake
+                            Lemon Meringue
+                            French Toast
+                            Lemon Sprinkle
+                            Cosmic Brownie
+                            Honey Vanilla Cruller
+                        </h1>
+                    </div>
+                    <h2 className="optionTwoPriceTitle">Crave Option 1 - Half Dozen</h2>
+                    <h3 className="optionTwoPrice">{optionOneThreePrice}</h3>
+                </Link>
+            </div>
+
+            {/* Option 2 Section */}
+            <div className="optionContainer">
+            <Link to="/productTwo" className='productLink'> {/* Link to productTwo Component */}
+                <div className="optionOneOption">
+                    <h1>
+                        Option 2: Half Dozen Donuts
+                        Flavors: 
+                        Birthday Cake
+                        Guava Passionfruit Cheesecake
+                        Maple Bacon
+                        Vanilla Sprinkle
+                        Carrot Cake Fritter
+                        Churro Cruller 
+                    </h1>
                 </div>
-                    <div className="optionContainer">
-                    <Link to="/productTwo" className='productLink'>
-                        <div className="optionTwoOption">
-                            <h1>
-                                Option 2: Half Dozen Donuts
-                                Flavors: 
-                                Birthday Cake
-                                Guava Passionfruit Cheesecake
-                                Maple Bacon
-                                Vanilla Sprinkle
-                                Carrot Cake Fritter
-                                Churro Cruller 
-                            </h1>
-                        </div>
-                        <h2 className="optionTwoPriceTitle">
-                            Crave Option 2 - Half Dozen
-                        </h2>
-                        <h3 className="optionTwoPrice">{optionOneThreePrice}</h3>
-                    </Link>
+                <h2 className="optionTwoPriceTitle">
+                    Crave Option 2 - Half Dozen
+                </h2>
+                <h3 className="optionTwoPrice">{optionOneThreePrice}</h3>
+            </Link>
+            </div>
+
+            {/* Option 3 Section */}
+            <div className="optionContainer">
+            <Link to="/productThree" className='productLink'> {/* Link to productThree Component */}
+                <div className="optionOneOption">
+                    <h1>
+                        Option 3: Vegan Half Dozen Donuts
+                        Flavors: 
+                        2x Oatmeal Cream Pie
+                        2x Biscoff Cookie Butter
+                        2x Chocolate PB + J
+                    </h1>
+                </div>
+                <h2 className="optionThreePriceTitle">
+                    Crave Option 3 - Half Dozen
+                </h2>
+                <h3 className="optionThreePrice">{optionOneThreePrice}</h3>
+            </Link>
+            </div>
+
+            {/* Option 4 Section */}
+            <div className="optionContainer">
+                <Link to="/productFour" className='productLink'> {/* Link to productFour Component */}
+                    <div className="optionOneOption">
+                        <h1>
+                            Option 4: One Dozen Donuts
+                            Flavors: 
+                            Birthday Cake
+                            Lemon Meringue
+                            Cosmic Brownie
+                            French Toast
+                            Maple Bacon
+                            Churro Cruller
+                            Carrot Cake Fritter 
+                            Honey Vanilla Cruller
+                            Chocolate PB + J
+                            Oatmeal Cream Pie
+                            Biscoff Cookie Butter
+                            Guava Passionfruit Cheesecake
+                        </h1>
                     </div>
-                    <div className="optionContainer">
-                    <Link to="/productThree" className='productLink'>
-                        <div className="optionThreeOption">
-                            <h1>
-                                Option 3: Vegan Half Dozen Donuts
-                                Flavors: 
-                                2x Oatmeal Cream Pie
-                                2x Biscoff Cookie Butter
-                                2x Chocolate PB + J
-                            </h1>
-                        </div>
-                        <h2 className="optionThreePriceTitle">
-                            Crave Option 3 - Half Dozen
-                        </h2>
-                        <h3 className="optionThreePrice">{optionOneThreePrice}</h3>
-                    </Link>
-                    </div>
-                    <div className="optionContainer">
-                        <Link to="/productFour" className='productLink'>
-                            <div className="optionFourOption">
-                                <h1>
-                                    Option 4: One Dozen Donuts
-                                    Flavors: 
-                                    Birthday Cake
-                                    Lemon Meringue
-                                    Cosmic Brownie
-                                    French Toast
-                                    Maple Bacon
-                                    Churro Cruller
-                                    Carrot Cake Fritter 
-                                    Honey Vanilla Cruller
-                                    Chocolate PB + J
-                                    Oatmeal Cream Pie
-                                    Biscoff Cookie Butter
-                                    Guava Passionfruit Cheesecake
-                                </h1>
-                            </div>
-                            <h2 className="optionFourPriceTitle">
-                                Crave Option 4 - One Dozen
-                            </h2>
-                            <h3 className="optionFourPrice">{optionTwoPrice}</h3>
-                        </Link>
-                </div> {/* optionOneContainer */}
-                <Footer />
+                    <h2 className="optionFourPriceTitle">
+                        Crave Option 4 - One Dozen
+                    </h2>
+                    <h3 className="optionFourPrice">44.00</h3>
+                </Link>
+            </div> {/* optionOneContainer */}
+
+            <Footer /> {/* Footer */}
         </div> // ProductComponentContent
      );
 }
