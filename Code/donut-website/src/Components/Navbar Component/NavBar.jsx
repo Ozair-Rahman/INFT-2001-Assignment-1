f13df444c0dc43a23187c1c91b3b56dc31ff61f2
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-02-05 10:33:41 -0500

    Index.css: Changed Font

--- Code/donut-website/src/Components/Navbar Component/NavBar.jsx
+++ Code/donut-website/src/Components/Navbar Component/NavBar.jsx
@@ -1,32 +1,33 @@
-import { Link } from "react-router-dom";
-import './navbar.css';
+// Imports
+import { Link } from "react-router-dom"; // Import Link So We Can Link Routes 
+import './navbar.css'; // Import CSS
 
 const NavBar = () => {
   return (
-    <div className="NavBar">
-      <nav>
-        <Link to='/'>
-          <img 
-          src="https://cdn.shopify.com/s/files/1/0273/3142/8435/files/CRAVE_wordmark_curve_180x.png?v=1614354953" 
-          alt="Home Logo"
-          />
+    <>
+    <nav>
+      <Link to='/'>
+        <img 
+        src="https://cdn.shopify.com/s/files/1/0273/3142/8435/files/CRAVE_wordmark_curve_180x.png?v=1614354953" 
+        alt="Home Logo"
+        />
+      </Link>
+      <div className="navbarLinks">
+        <Link to="/" className="HomeLink">
+          <label>Home</label>
         </Link>
-        <div className="navbarLinks">
-          <Link to="/" className="HomeLink">
-            <label>Home</label>
-          </Link>
-          <Link to="/about" className="AboutLink">
-            <label>About</label>
-          </Link>
-          <Link to="/product" className="ProductLink">
-            <label>Product</label>
-          </Link>
-          <Link to="/contact" className="ContactLink">
-            <label>Contact US!</label>
-          </Link>
-        </div>  
-      </nav>
-    </div>
+        <Link to="/about" className="AboutLink">
+          <label>About</label>
+        </Link>
+        <Link to="/product" className="ProductLink">
+          <label>Product</label>
+        </Link>
+        <Link to="/contact" className="ContactLink">
+          <label>Contact US!</label>
+        </Link>
+      </div>  
+    </nav>
+    </>
   );
 };
 
