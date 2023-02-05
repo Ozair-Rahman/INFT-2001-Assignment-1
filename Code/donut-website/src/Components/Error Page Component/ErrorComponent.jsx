b2d11b69da460814dfcc861b6e32dbbe50d1c3f0
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-02-05 12:41:18 -0500

    Error Page Component: Added comments to Error Page Component

--- Code/donut-website/src/Components/Error Page Component/ErrorComponent.jsx
+++ Code/donut-website/src/Components/Error Page Component/ErrorComponent.jsx
@@ -1,23 +1,28 @@
-import { Link } from 'react-router-dom';
+// Imports
+import { Link } from 'react-router-dom'; // Import Link from react-router to redirect pages
 import Footer from '../Footer Component/Footer';
 import NavBar from '../Navbar Component/NavBar';
 import './ErrorComponent.css';
 
 const ErrorComponent = () => {
 
-    document.title = 'Error 404 No Page Found - Crave Doughnuts';
+    document.title = '404 Not Found - Crave Doughnuts'; // Change Title When On Error Page
 
     return ( 
         <>
-            <NavBar />
-            <div className="erroMsg">
-                <h1 className='errorHeading'>Error 404 - No Page Found</h1>
-                <p className='errorSubHeading'>The page you requested does not exist.</p>
-                <Link to='/' className='homeLink'>
-                    <p className='homeLinkText'>Continue Shopping</p>
-                </Link>
-            </div>
-            <Footer />
+        <NavBar /> {/* navBar */}
+
+        {/* errorMsg Div */}
+        <div className="erroMsg">
+            <h1 className='errorHeading'>404 Page Not Found</h1>
+            <p className='errorSubHeading'>The page you requested does not exist.</p>
+
+            {/* Redirect to Homepage */}
+            <Link to='/' className='homeLink'>
+                <p>Continue Shopping</p>
+            </Link>
+        </div>
+        <Footer /> {/* Footer */}
         </>
      );
 }
