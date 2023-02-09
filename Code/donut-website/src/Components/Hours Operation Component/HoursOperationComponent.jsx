eaf0f3e1797725fa3c0b0a712a652689773bdb09
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-02-09 13:41:48 -0500

    Fixed Hours of Operation Stylinng

--- Code/donut-website/src/Components/Hours Operation Component/HoursOperationComponent.jsx
+++ Code/donut-website/src/Components/Hours Operation Component/HoursOperationComponent.jsx
@@ -3,12 +3,14 @@ import './HoujsOperationComponent.css';
 const HoursOperationComponent = () => {
     return (
         <>
-        <h1 className="hoHeading">Hours of Operation</h1>
-        <h2 className="daysHeading">Wednesday - Sunday </h2>
-        <p className="hoursHeading">8-5 PM OR SOLD OUT!</p>
-        <p className="hoursHeading">8-9 AM PREORDER PICKUP ONLY</p>
-        <h2>Walk Ins</h2>
-        <p className="walkinSubHeading">9-5 PM OR UNTIL SOLD OUT!</p>
+        <div className="operationHours">
+            <h1 className="hoHeading">Hours of Operation</h1>
+            <h2 className="heading">Wednesday - Sunday </h2>
+            <p className="hoursHeading">8-5 PM OR SOLD OUT!</p>
+            <p className="hoursHeading">8-9 AM PREORDER PICKUP ONLY</p>
+            <h2 className='walkInHeading'>Walk Ins</h2>
+            <p className="walkinSubHeading">9-5 PM OR UNTIL SOLD OUT!</p>
+        </div>
         </>
     );
 }
